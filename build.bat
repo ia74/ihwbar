0436607aac700103dbeb4df9faf8ff825b15f3de
Aiden <aidenjs9@icloud.com>
2024-05-12 20:04:42 -0500

    make build process cooler

--- build.bat
+++ build.bat
@@ -3,9 +3,13 @@
 color 0a
 cls
 
+mkdir build
+mkdir bin
+cd build
+
 :PACK_RESOURCES
 echo Packing resources...
-rc /nologo /r /fo bar.res src/bar.rc
+rc /nologo /r /fo bar.res ../src/bar.rc
 if errorlevel 1 goto :ERROR
 goto :COMPILE
 
@@ -16,7 +20,12 @@ exit /b %ERRORLEVEL%
 
 :COMPILE
 echo Compiling...
-cl /nologo /EHsc User32.lib Gdi32.lib src/main.cpp  src/widgets/WidgetText.cpp src/Boot.cpp src/BarConfiguration.cpp bar.res
+cl /nologo /EHsc User32.lib Gdi32.lib ^
+	../src/main.cpp ^
+	../src/widgets/WidgetText.cpp ^
+	../src/Boot.cpp ^
+	../src/BarConfiguration.cpp ^
+	bar.res
 if errorlevel 1 goto :ERROR
 goto :RENAME
 
@@ -28,13 +37,14 @@ goto :MOVE_TO_BIN
 
 :MOVE_TO_BIN
 echo Moving to bin...
-move IHWBar.exe bin
+move IHWBar.exe ../bin
 if errorlevel 1 goto :ERROR
 goto :RUN
 
 :RUN
 color 07
 echo Running...
+cd ..
 start bin/IHWBar.exe
 if errorlevel 1 goto :ERROR
 goto :EOF
